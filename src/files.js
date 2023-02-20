9072f4c813040bb51d8c8a7520d1ff29e76b0b5c
elenashion <edgir@bk.ru>
2023-02-20 17:40:14 +0300

    Refactor messages container, add resizing, add textarea for adding content, change some styles

--- src/files.js
+++ src/files.js
@@ -2,8 +2,15 @@ const app = new Vue({
   vuetify: new Vuetify({
   }),
   el: '#app',
+  async beforeDestroy () {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.onResize, { passive: true })
+    }
+  },
   async mounted () {
+    await this.onResize();
     await this.fetchData();
+    window.addEventListener('resize', this.onResize, { passive: true });
   },
   methods: {
     async fetchData () {
@@ -16,10 +23,19 @@ const app = new Vue({
 
     async addNewFile () {
       // TODO
+    },
+
+    async onResize () {
+      // Minus app-bar and margins
+      this.windowsHeights.filesListHeight = window.innerHeight - 75;
+      this.windowsHeights.filesEditPanelHeight = this.windowsHeights.filesListHeight / 4;
+      this.windowsHeights.filesContentHeight = this.windowsHeights.filesEditPanelHeight * 3;
+      // Minus fixed-sized files list card and margins
+      this.filesEditPanelWeight = window.innerWidth - 350 - 20;
     }
   },
   data: {
-    items: null,
+    items: [],
     checkedItem: null,
     color: 'rgb(232, 243, 247)',
   }
